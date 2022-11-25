641cdc2dc16613e3843bdf7d5070bef65a08e621
unipen_1_1.pyunipen2022 <unipen_1_1.pyunipen2022@gmail.com>
2022-11-25 11:03:45 +0100

    2. feladat

--- EU/main.cpp
+++ EU/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
-
+struct orszag
+{
+    string  nev         ;
+    int     belepes     ;
+    string nyelv        ;
+};
 int main()
 {
-    cout << "EU" << endl;
+    cout << "EU" << endl            ;
+    orszag orszagok[50]             ;
+    ifstream be("c:\\c++\\eu.txt")  ;
+    string nev      ;
+    int belepes     ;
+    string nyelv    ;
+    while(be >> nev >> belepes >> nyelv)
+    {
+        cout << nev << endl ;
+    }
     return 0;
 }
