1016892acb52fb311c74a11cb1d66e7086b1e9db
unipen_1_1.pyunipen2022 <unipen_1_1.pyunipen2022@gmail.com>
2022-11-25 11:09:06 +0100

    2. feladat

--- EU/main.cpp
+++ EU/main.cpp
@@ -16,9 +16,18 @@ int main()
     string nev      ;
     int belepes     ;
     string nyelv    ;
+    int sor = 0     ;
     while(be >> nev >> belepes >> nyelv)
     {
-        cout << nev << endl ;
+        orszagok[sor].nev       = nev       ;
+        orszagok[sor].belepes   = belepes   ;
+        orszagok[sor].nyelv     = nyelv     ;
+        sor++                               ;
+    }
+    cout << "2. feladat" << endl ;
+    for (int i = 0 ; i < sor ; i++)
+    {
+        cout << "Orszag:" << orszagok[i].nev << " Belepes eve:" << orszagok[i].belepes << endl ;
     }
     return 0;
 }
